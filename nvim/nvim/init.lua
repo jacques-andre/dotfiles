597d3d8725ff35498dcc51e741f4acb6f688bca5
jacques-andre <jacquesandre@mailbox.org>
2023-11-01 14:23:50 +0000

    chore: add auto spell

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -29,18 +29,8 @@ vim.keymap.set('n', '<leader>t', api.tree.toggle, {})
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- local function my_on_attach(bufnr)
---   local api = require "nvim-tree.api"
-
---   local function opts(desc)
---     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---   end
-
---   api.config.mappings.default_on_attach(bufnr)
--- end
-
--- require("nvim-tree").setup {
---   on_attach = my_on_attach,
--- }
+-- auto enable spell
+vim.cmd [[autocmd FileType markdown setlocal spell]]
+vim.cmd [[autocmd FileType gitcommit setlocal spell]]
 
 require("nvim-tree").setup()
